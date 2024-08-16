78b1095f46ab280287bffddc6efd4a28ea661def
Acc3ss Violation <wrdbrookhuis@gmail.com>
2024-08-16 21:34:58 +0200

    Update flash script for Arduino as ISP

--- Controller/flash.sh
+++ Controller/flash.sh
@@ -3,4 +3,8 @@
 OUT="controller"
 HEX="build/${OUT}.hex"
 
-avrdude -v -p atmega328p -c arduino -P /dev/ttyACM0 -b 115200 -U flash:w:build/${OUT}.hex:i
+# Arduino UNO
+# avrdude -v -p atmega328p -c arduino -P /dev/ttyACM0 -b 115200 -U flash:w:build/${OUT}.hex:i
+
+# Arduino as ISP
+avrdude -v -p atmega328p -c stk500v1 -P /dev/ttyACM0 -b 19200 -U flash:w:build/${OUT}.hex:i
