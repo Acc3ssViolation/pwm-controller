abe4bf12d8ece697110774324caf2c770a927fe2
Acc3ss Violation <wrdbrookhuis@gmail.com>
2024-08-17 15:56:41 +0200

    Set up LMD18200 pins

--- Controller/src/pwm_driver.c
+++ Controller/src/pwm_driver.c
@@ -4,103 +4,78 @@
 #include <avr/io.h>
 
 const uint16_t PWM_TOP = 0x2FF;
-const gpio_info_t PIN_ENABLED = {.port = GPIO_PORT_B, .pin = GPIO_PIN_5};
+const gpio_info_t PIN_DIR = {.port = GPIO_PORT_B, .pin = GPIO_PIN_2};       // OC1B
+const gpio_info_t PIN_BRAKE = {.port = GPIO_PORT_D, .pin = GPIO_PIN_5};     // OC0B
+const gpio_info_t PIN_PWM = {.port = GPIO_PORT_D, .pin = GPIO_PIN_6};       // OC0A
+const gpio_info_t PIN_NTHERMAL = {.port = GPIO_PORT_D, .pin = GPIO_PIN_4};  // PCINT20
 
-static bool m_enabled;
-static bool m_reversed;
-static uint8_t m_dutyCycle;
-
-static void set_tccr1a(void);
+// LMD18200 truth table
+// From TI datasheet page 8
+// PWM  DIR  BRK  OUT
+// 1    1    0    Source 1, Sink 2        Out 1 V+
+// 1    0    0    Sink 1, Source 2        Out 2 V+
+// 0    X    0    Source 1, Source 2      Both tied to V+
+// 1    1    1    Source 1, Source 2      Both tied to V+
+// 1    0    1    Sink 1, Sink 2          Both tied to V-
+// 0    X    1    None                    Outputs disconnected
+//
+// From this table we can conclude the following operating modes:
+// For DCC we want to PWM on the DIR pin, which will give complementary outputs on Out 1 and Out 2  (PWM = 1, BRK = 0)
+// For analog PWM we can PWM on the BRK pin, which will PWM one of the outputs while keeping the other grounded (PWM = 1, DIR = DIR)
+// For analog PWM we can also PWM on the PWM pin instead, leaving BRK set to 0. This means both outputs are set to V+ instead of V- when "disabled"
+//
+// The minimum time between changing different pins should be 1 microsecond.
 
 void pwm_driver_initialize(void)
 {
-  gpio_reset_pin(PIN_ENABLED.port, PIN_ENABLED.pin);
-  gpio_configure_output(PIN_ENABLED.port, PIN_ENABLED.pin);
+  // Disable outputs on startup
+  gpio_set_pin(PIN_BRAKE.port, PIN_BRAKE.pin);
+  gpio_configure_output(PIN_BRAKE.port, PIN_BRAKE.pin);
+  gpio_reset_pin(PIN_PWM.port, PIN_PWM.pin);
+  gpio_configure_output(PIN_PWM.port, PIN_PWM.pin);
+  gpio_reset_pin(PIN_DIR.port, PIN_DIR.pin);
+  gpio_configure_output(PIN_DIR.port, PIN_DIR.pin);
 
-  // OC1A (PB1) and OC1B (PB2) are used as output
-  gpio_reset_pin(GPIO_PORT_B, GPIO_PIN_1);
-  gpio_reset_pin(GPIO_PORT_B, GPIO_PIN_2);
-  gpio_configure_output(GPIO_PORT_B, GPIO_PIN_1);
-  gpio_configure_output(GPIO_PORT_B, GPIO_PIN_2);
+  // Pull up
+  gpio_set_pin(PIN_NTHERMAL.port, PIN_NTHERMAL.pin);
 
-  // Set up the timer
-  // We use Fast PWM mode using ICR1 as the max value. We don't use a prescaler
-  // so we end up with F_CPU / 0x2FF which ends up at 20860.49... Hz for a 16 MHz
-  // clock frequency. This gives us about 9.5 bits worth of resolution.
-  ICR1 = PWM_TOP;
+  // // Set up the timer
+  // // We use Fast PWM mode using ICR1 as the max value. We don't use a prescaler
+  // // so we end up with F_CPU / 0x2FF which ends up at 20860.49... Hz for a 16 MHz
+  // // clock frequency. This gives us about 9.5 bits worth of resolution.
+  // ICR1 = PWM_TOP;
 
-  gpio_reset_pin(PIN_ENABLED.port, PIN_ENABLED.pin);
-  TCCR1B = BIT(WGM13) | BIT(WGM12) | BIT(CS10);
-  TCCR1A = BIT(WGM11);
+  // gpio_reset_pin(PIN_ENABLED.port, PIN_ENABLED.pin);
+  // TCCR1B = BIT(WGM13) | BIT(WGM12) | BIT(CS10);
+  // TCCR1A = BIT(WGM11);
 }
 
 void pwm_driver_set_enabled(bool enabled)
 {
-  if (m_enabled == enabled)
+  if (enabled)
   {
-    return;
-  }
-
-  m_enabled = enabled;
-  if (m_enabled)
-  {
-    gpio_set_pin(PIN_ENABLED.port, PIN_ENABLED.pin);
+    gpio_set_pin(PIN_PWM.port, PIN_PWM.pin);
+    gpio_reset_pin(PIN_BRAKE.port, PIN_BRAKE.pin);
   }
   else
   {
-    gpio_reset_pin(PIN_ENABLED.port, PIN_ENABLED.pin);
+    gpio_reset_pin(PIN_PWM.port, PIN_PWM.pin);
+    gpio_set_pin(PIN_BRAKE.port, PIN_BRAKE.pin);
   }
-
-  set_tccr1a();
 }
 
 void pwm_driver_set_duty_cycle(uint8_t dutyCycle)
 {
-  if (m_dutyCycle == dutyCycle)
-  {
-    return;
-  }
-
-  // Map the value to our full range
-  uint32_t tmp = dutyCycle;
-  tmp *= 0x2FFUL;
-  tmp /= 0xFFUL;
-
-  OCR1A = (uint16_t)tmp;
-  OCR1B = (uint16_t)tmp;
-
-  m_dutyCycle = dutyCycle;
-
-  set_tccr1a();
 }
 
 void pwm_driver_set_reversed(bool reversed)
 {
-  if (reversed == m_reversed)
+  if (reversed)
   {
-    return;
+    gpio_reset_pin(PIN_DIR.port, PIN_DIR.pin);
   }
-
-  m_reversed = reversed;
-
-  set_tccr1a();
-}
-
-static void set_tccr1a(void)
-{
-  if (!m_enabled || m_dutyCycle == 0)
+  else
   {
-    // Channel B and channel A are regular output set to ground
-    TCCR1A = BIT(WGM11);
-  }
-  else if (m_reversed) 
-  {
-    // Channel B is PWM output, channel A is regular output set to ground
-    TCCR1A = BIT(COM1B1) | BIT(WGM11);
-  }
-  else 
-  {
-    // Channel A is PWM output, channel B is regular output set to ground
-    TCCR1A = BIT(COM1A1) | BIT(WGM11);
+    gpio_set_pin(PIN_DIR.port, PIN_DIR.pin);
   }
 }
